ac65702308e8029e8f4a6489001131ec23779076
oliverloops <oliveralr@protonmail.com>
2021-02-20 11:08:28 -0600

    Updating layout

--- src/components/SearchFilter.js
+++ src/components/SearchFilter.js
@@ -1,17 +1,22 @@
+import { GrSearch } from "react-icons/gr";
+
 const SearchFilter = ({ props }) => {
   const getField = (e) => {
     console.log(e.target.value);
   };
 
   return (
-    <form>
-      <input
-        type="text"
-        placeholder="Search for science"
-        value={props}
-        onChange={(e) => getField(e)}
-      />
-    </form>
+    <div className="search-container">
+      <form>
+        <GrSearch className="search-icon" size={20} />
+        <input
+          type="text"
+          placeholder="Search for science"
+          value={props}
+          onChange={(e) => getField(e)}
+        />
+      </form>
+    </div>
   );
 };
 
